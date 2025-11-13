0787909eadf23b69193b13cf8d0f7f04f5760ec2
Pat-devs <patryk.kodehode@gmail.com>
2025-11-13 09:51:14 +0100

    objects examples

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// let userName = "Patryk"
+
+// let userAge = 12
+
+// let userWeight = 90.5
+
+// let userHobbies = ["coding", "walking", "eating"]
+
+// let userGreet = function() {
+//     console.log("Hi there!")
+// }
+
+// userGreet()
+
+
+// function greeting() {
+//     console.log("Hello everyone!")
+// }
+
+
+
+/* OBJECTS */
+
+let userDetails = {
+    name: "Patryk",
+    age: 11,
+    weight: 90.5,
+    hobbies: ["coding", "walking", "eating"],
+    greet: function() {
+        console.log("Hi")
+    }
+}
+
+console.log(userDetails.age)
+console.log(userDetails.hobbies)
+
+userDetails.greet()
+
+
+console.log()
+
+document.querySelector()
